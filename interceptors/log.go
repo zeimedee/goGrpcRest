07ff9b6814d9db2b0b7833eb68f757ec4bcfe5bb
alexander <zeimedee@gmail.com>
2022-01-27 22:46:20 +0000

    crud grpc server with server interceptor setup

--- interceptors/log.go
+++ interceptors/log.go
@@ -3,6 +3,7 @@ package interceptors
 import (
 	"context"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -13,8 +14,9 @@ func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 		req interface{},
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler) (resp interface{}, err error) {
+		start := time.Now()
 
-		log.Println("logging....")
+		log.Println(info.FullMethod, time.Since(start), req)
 		res, err := handler(ctx, req)
 
 		return res, err
